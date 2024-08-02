fbda51fe40a1feb4b3b191b8b21925d652706d31
ah7255703 <ah7255703@gmail.com>
2024-08-02 19:23:12 +0300

    chore: Update Editor module and dependencies

--- apps/web/src/Editor/Editor.tsx
+++
@@ -1,9 +0,0 @@
-import EditorManager from "./EditorManager";
-import { StoreRoot } from "@scena/react-store";
-
-
-export default function Editor() {
-    return <StoreRoot>
-        <EditorManager />
-    </StoreRoot>;
-}
--- apps/web/src/routes/(authenticated)/_layout-2/index.tsx
+++ apps/web/src/routes/(authenticated)/_layout-2/index.tsx
@@ -1,8 +1,6 @@
-import Editor from '@/Editor/Editor'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(authenticated)/_layout-2/')({
   component: () => <div className='h-screen w-full'>
-    <Editor />
   </div>
 })
